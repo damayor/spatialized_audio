43ef36d5d5dea5aebb404b75ad5427a46c202e58
damayor <dr.mayorga20@uniandes.edu.co>
2016-11-28 00:47:37 -0500

    audio reverbed inside church

---
+++ Assets/Scripts/AudioZone.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioZone : MonoBehaviour {
+
+
+	AudioSource audio;
+
+	public AudioClip campanas;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		audio = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void onTriggerEnter (Collider other)
+    {
+		//if(other.gameObject.name.Contains("organo"))
+		{ 
+			//SetControlArbol(other.gameObject.GetComponent<ControlArbol>());
+			//aSource.PlayOneShot(xmas);
+			audio.enabled = true;
+			audio.PlayOneShot(campanas);
+		}
+
+    }
+
+    void onTriggerExit(Collider other)
+    {
+		if (other.gameObject.name.Contains ("organo")) {
+			audio.Stop ();
+		}
+
+    }
+
+}
